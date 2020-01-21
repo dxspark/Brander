cfa949a1818e27d17f4bedb6b5363088e31a5131
nicruo <nicruo@gmail.com>
2020-01-21 19:09:01 +0000

    Added first files;

---
+++ Brander.Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Brander.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Constants.GreetingMessage);
+            Console.WriteLine(DateTime.Now.ToString(Constants.DateFormat));
+
+            Console.WriteLine(File.ReadAllText("ASCIIImage.txt"));
+        }
+    }
+}
---
+++ Brander/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+
+namespace Brander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string branderFile = args[0];
+            string branderDir = Directory.GetParent(branderFile).FullName;
+
+
+            JObject branderConfig = JObject.Parse(File.ReadAllText(branderFile));
+
+            foreach (var template in branderConfig["templates"])
+            {
+                var templateSourcePath = Path.Combine(branderDir, template["source"].Value<string>());
+                
+            }
+
+            var rep = Regex.Replace("hello {{greetingMessage}} you are so cool {{sss}})", @"\{{([a-zA-Z0-9]*)}}", (m) =>
+             {
+                 if(m.Groups != null && m.Groups.Count > 0)
+                 {
+                     JToken variableValue = branderConfig["variables"][m.Groups[1].Value];
+                     if (variableValue != null)
+                         return variableValue.Value<string>();
+                 }
+                 return string.Empty;
+             });
+
+            Console.WriteLine(rep);
+        }
+    }
+}
