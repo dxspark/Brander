18569fbfe554799761ac42de4b9d985d8acda362
nicruo <nicruo@gmail.com>
2020-01-22 13:38:17 +0000

    Added template functionality;

--- Brander/Program.cs
+++ Brander/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json.Linq;
 
@@ -9,31 +8,34 @@ namespace Brander
     {
         static void Main(string[] args)
         {
-
             string branderFile = args[0];
             string branderDir = Directory.GetParent(branderFile).FullName;
 
-
             JObject branderConfig = JObject.Parse(File.ReadAllText(branderFile));
 
-            foreach (var template in branderConfig["templates"])
+            if (branderConfig["templates"] != null)
             {
-                var templateSourcePath = Path.Combine(branderDir, template["source"].Value<string>());
-                
+                foreach (JObject template in branderConfig["templates"])
+                {
+                    string templateSourcePath = Path.Combine(branderDir, template["source"].Value<string>());
+                    string templateSourceContent = File.ReadAllText(templateSourcePath);
+
+                    string finalContent = Regex.Replace(templateSourceContent, @"\{{([a-zA-Z0-9]*)}}", (m) =>
+                    {
+                        if (m.Groups != null && m.Groups.Count > 0)
+                        {
+                            JToken variableValue = branderConfig["variables"][m.Groups[1].Value];
+                            if (variableValue != null)
+                                return variableValue.Value<string>();
+                        }
+                        return string.Empty;
+                    });
+
+                    string templateTargetPath = Path.Combine(branderDir, template["target"].Value<string>());
+                    File.WriteAllText(templateTargetPath, finalContent);
+                }
             }
 
-            var rep = Regex.Replace("hello {{greetingMessage}} you are so cool {{sss}})", @"\{{([a-zA-Z0-9]*)}}", (m) =>
-             {
-                 if(m.Groups != null && m.Groups.Count > 0)
-                 {
-                     JToken variableValue = branderConfig["variables"][m.Groups[1].Value];
-                     if (variableValue != null)
-                         return variableValue.Value<string>();
-                 }
-                 return string.Empty;
-             });
-
-            Console.WriteLine(rep);
         }
     }
 }
